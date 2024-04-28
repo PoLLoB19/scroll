76a197fbf8dd271a58babc91ee199dfc87e680cf
PoLLoB19 <pollobatwork19@gmail.com>
2024-04-28 15:35:11 +0600

    dfgh

---
+++ script.js
@@ -0,0 +1,15 @@
+const div1 =document.querySelector(".div1");
+const div2 =document.querySelector(".div2");
+
+
+    document.addEventListener('scroll',()=>{
+    div1.classList.add('div1an');
+    div2.classList.add("div2an");
+});
+
+
+
+
+
+
+
